e0184f3acf5b8ee22934e344890db10a6d2c2868
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-03-02 19:32:02 +0530

    Creating spring boot project

---
+++ src/main/java/com/mugundhan/spring_liquibase/liquibase_learn/LiquibaseLearnApplication.java
@@ -0,0 +1,13 @@
+package com.mugundhan.spring_liquibase.liquibase_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugundhan/spring_liquibase/liquibase_learn/LiquibaseLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugundhan.spring_liquibase.liquibase_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
